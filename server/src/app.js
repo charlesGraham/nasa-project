cbdbdc48d290848756f0d3ad31e6866b427c9200
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-30 14:30:48 -0600

    Added morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets.router");
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(cors({
     orign: "http:127.0.0.1:3000"
 }));
+
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(planetsRouter);
 

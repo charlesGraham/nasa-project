4ea905e9af0331f9cdbf3c8cac35dac99d07d704
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-30 13:34:28 -0600

    Added CORS

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 
 const planetsRouter = require("./routes/planets.router");
 
 const app = express();
+
+app.use(cors({
+    orign: "http:127.0.0.1:3000"
+}));
 app.use(express.json());
 app.use(planetsRouter);
 

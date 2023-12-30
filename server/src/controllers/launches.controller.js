1444d40d22bcac1e481e4d800b38fbf964159402
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-30 14:40:44 -0600

    Formatted output from getAllLaunches()

--- server/src/controllers/launches.controller.js
+++ server/src/controllers/launches.controller.js
@@ -2,7 +2,7 @@ const { launches } = require("../models/launches.model");
 
 
 const getAllLaunches = (req, res) => {
-    return res.status(200).json(launches);
+    return res.status(200).json(Array.from(launches.values()));
 }
 
 module.exports = {

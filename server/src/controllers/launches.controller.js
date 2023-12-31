94044bcc329cd2004950cc329211c5233b8948f7
Charles Graham <15911636+charlesGraham@users.noreply.github.com>
2023-12-31 11:13:10 -0600

    Added if-checks to validate inputs

--- server/src/controllers/launches.controller.js
+++ server/src/controllers/launches.controller.js
@@ -7,7 +7,20 @@ const httpGetAllLaunches = (req, res) => {
 
 const httpAddNewLaunch = (req, res) => {
     const launch = req.body;
+
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destionation) {
+        return res.status(400).json({
+            error: "Missing launch property."
+        });
+    }
+
     launch.launchDate = new Date(launch.launchDate);
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: "Invalid launch date"
+        });
+    }
+
     addNewLaunch(launch);
 
     return res.status(201).json(launch);
